570fe30f16037310c8c758db42a1e496649fcb05
YUVRAJ <113570060+Chauhan-yuvraj@users.noreply.github.com>
2025-04-29 16:39:22 +0530

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { Text } from 'react-native'
 
 export default function TabsLayout() {
   return (
-  <Tabs>
-    <Tabs.Screen name="Home" />
-  </Tabs>
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerTitle: "👋 Hey! What's up?",
+          tabBarIcon: () => <Text style={{ fontSize: 20 }}>🏠</Text>, // Custom emoji icon
+        }}
+      />
+    </Tabs>
   )
 }
